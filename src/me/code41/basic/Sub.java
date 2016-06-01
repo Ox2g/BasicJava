ae0c80516827f9cd15e3e204b3bf5153ae0c4005
liushiyao <liushiyao@jd.com>
2016-06-01 10:09:00 +0800

    basic

---
+++ src/me/code41/basic/Sub.java
@@ -0,0 +1,24 @@
+package me.code41.basic;
+
+/**
+ * Created by liushiyao on 2016/6/1.
+ */
+public class Sub {
+    public Sub() {
+        test();
+    }
+    public void test() {
+        System.out.println("three");
+    }
+    public static class Child extends Sub {
+        private static int a = 3;
+
+        public void test() {
+            System.out.println(a);
+        }
+    }
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.test();
+    }
+}
