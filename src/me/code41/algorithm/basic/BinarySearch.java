315a75667c695a84507673cca23345a34536362a
liushiyao <liushiyao@jd.com>
2016-06-06 15:58:39 +0800

    basic sort

--- src/me/code41/algorithm/basic/BinarySearch.java
+++ src/me/code41/algorithm/basic/BinarySearch.java
@@ -6,7 +6,7 @@ package me.code41.algorithm.basic;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] array = new int[]{1, 3, 4, 5, 8, 9, 11, 23, 45};
-        System.out.println(binarySearch(array, 10));
+        System.out.println(binarySearch2(array, 11));
     }
 
     public static int binarySearch(int[] array, int target) {
@@ -20,6 +20,22 @@ public class BinarySearch {
                 high = middle - 1;
             } else {
                 low = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch2(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            if (target == array[middle]) {
+                return middle;
+            } else if (target > array[middle]) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
             }
         }
         return -1;
